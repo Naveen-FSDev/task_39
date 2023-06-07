d3e5d3cd9f6b3ddf61c02ca4f93c7bf6eecdf6c1
naveenfsdev <naveensrpce@gmail.com>
2023-06-07 07:09:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const fs =require("fs");
+const dotenv = required("dotenv");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+app.get("/",(req,res)=>{
+    res.send({filepath:{
+        home : "/",
+        create_new_file : "/createfile",
+        read_file : "/readfile"
+    }})
+});
+
+//create new data and time
+
+let current = new DataTransfer();
+let cdate =
+current.getFullYear() +
+"-" +
+(current.getMonth()+ 1)+
+"-" +
+current.getDate();
+
+let cTime =
+current.getHours() + "." + current.getMinutes() + "." + current.getSeconds();
+let dateTime = cDate + " " + cTime;
+console.log(dateTime);
+
+//create new file
+
+ app.get ("/createfile", (req,res)=>{
+
+    fs.writeFile(`./files/${dateTime}.doc`,dateTime,(err)=> {
+        console.log (`${dateTime}.doc created successfully`);
+    });
+
+    res.send({msg: `${dateTime}.doc created successfully`});
+ });
+
+ //read file
+
+ app.get("/readfile",(req,res) => {
+
+    fs.readdir ("files", (err,data) => {
+        if (err) {
+            return res.status(404).send(err);
+        } else {
+            return res.status(200).send({files:data});
+        }
+    });
+ });
+
+ app.listen(PORT,()=>{console.log(`App is running on port : ${PORT}`)})
